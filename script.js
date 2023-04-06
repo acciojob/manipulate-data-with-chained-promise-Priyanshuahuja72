8e5e197be42e25d729a48fed7b3711594577def9
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-06 16:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+function oddNumber(arr)
+{
+ let odd = arr.filter((num) => num%2 == 1)
+return odd;
+}
+function evenNumber(arr)
+{
+  let even = arr.filter((num) => num%2 == 0);
+  let product = 1;
+  for (let i = 0; i < even.length; i++) {
+    product *= even[i];
+  }
+  return product;
+}
+function promise1(arr)
+{
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+    resolve(
+    document.getElementById('output').innerHTML = `${oddNumber(arr)}`
+    );
+    },1000)
+  })
+}
+function promise2(arr)
+{
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+     resolve(
+     document.getElementById('output').innerHTML = `${evenNumber(arr)}`
+     );
+    },2000)
+  })
+}
+let button = document.getElementById("sub");
+button.addEventListener("click" , () => {
+    let aval = document.getElementById("input-val").value;
+    let arr = aval.split(',');
+    Promise.all([promise1(arr) , promise2(arr)])
+})

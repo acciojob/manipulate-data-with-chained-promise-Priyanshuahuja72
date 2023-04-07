2d7b8ffbcb8ad4608ebeffc0c45db54c6a9ebb88
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-07 11:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 //your JS code here. If required.
-function oddNumber(arr)
-{
- let odd = arr.filter((num) => num%2 == 1)
-return odd;
-}
-function evenNumber(arr)
-{
-  let even = arr.filter((num) => num%2 == 0);
-  let product = 1;
-  for (let i = 0; i < even.length; i++) {
-    product *= even[i];
-  }
-  return product;
-}
-function promise1(arr)
-{
-  return new Promise((resolve , reject) => {
+const output = document.getElementById("output");
+const array = [1, 2, 3, 4];
+
+function filterEven() {
+  return new Promise((resolve, reject) => {
+    const evenArray = array.filter((e) => e % 2 == 0);
     setTimeout(() => {
-    resolve(
-    document.getElementById('output').innerHTML = `${oddNumber(arr)}`
-    );
-    },1000)
-  })
+      output.innerText = `${evenArray}`;
+      resolve(evenArray);
+    }, 1000);
+  });
 }
-function promise2(arr)
-{
-  return new Promise((resolve , reject) => {
+
+function multiplyByTwo(val) {
+  return new Promise((resolve, reject) => {
+    const result = val.map((e) => e * 2);
     setTimeout(() => {
-     resolve(
-     document.getElementById('output').innerHTML = `${evenNumber(arr)}`
-     );
-    },2000)
-  })
+      output.innerText = `${result}`;
+      resolve(result);
+    }, 2000);
+  });
 }
-let button = document.getElementById("sub");
-button.addEventListener("click" , () => {
-    let aval = document.getElementById("input-val").value;
-    let arr = aval.split(',');
-    Promise.all([promise2(arr) , promise1(arr)])
-})
+
+filterEven().then((val) => multiplyByTwo(val));
